bd036ddf4a83f03b03ec602c27de1231c047e20c
Richard Hsieh <monaludao@gmail.com>
2015-11-27 12:11:20 +0800

    update

--- data_handler.R
+++ data_handler.R
@@ -2,6 +2,7 @@ data_handle<-function(file.num){
     file.num=1
     
     file.list<-dir("./tpdata")
+    file.list<-file.list[grep(".csv",file.list)]
     file.path<-paste0("./tpdata/",file.list)
     
     csv.df<-read.csv(file.path[file.num],stringsAsFactors = FALSE)
@@ -10,13 +11,19 @@ data_handle<-function(file.num){
     for (i in 1:nrow(csv.df)){
         csv.df$CID[i]<-check_CID(csv.df$CID[i])
     }
-                       
-    csv.df$village<-gsub(".*區(.*里).*","\\1",csv.df[,4])
-    csv.df$village<-as.character(csv.df$village)
+
+    csv.df[grep("<U+",csv.df[,4]),4]<-"找不到指定的門牌地址。:UTF16"
+                           
+    csv.df$city<-as.factor(gsub("^(.*(市|縣)).*","\\1",csv.df[1,4]))
+    csv.df$dist<-as.factor(gsub(".*市(.*區).*","\\1",csv.df[1,4]))
+    csv.df$village<-as.factor(gsub(".*區(.*里).*","\\1",csv.df[,4]))
+    csv.df$neighbour<-as.factor(gsub(".*里(.*鄰).*","\\1",csv.df[,4]))
+    csv.df$road<-as.factor(gsub(".*鄰(.*(路|街|大道)).*","\\1",csv.df[,4]))
     
-    csv.df$neighbour<-gsub(".*里(.*鄰).*","\\1",csv.df[,4])
-    csv.df$neighbour<-as.character(csv.df$neighbour)
-    
+    csv.df$sec<-NA
+    for (i in 1:nrow(csv.df)){
+        csv.df$sec[i]<-check_section(csv.df$Response_Address)
+    }
     
 }
 
@@ -25,4 +32,13 @@ check_CID<-function(CID){
         CID<-paste0(paste(rep("0",(8-nchar(CID))),collapse = ""),CID)
     }
     return(CID)
+}
+
+check_section<-function(address){
+    if(grepl("段",address)){
+        section<-gsub(".*(路|街|大道)(.*段).*","\\2",address)
+    } else {
+        section<-NA
+    }
+    return(section)
 }
